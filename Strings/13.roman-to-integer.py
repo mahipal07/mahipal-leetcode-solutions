e2e86ecc56c21404e111d12e4d01e8372a88b8cc
Mahipal Singh <mahipal.singh1k72@gmail.com>
2026-06-25 11:29:03 +0530

    LeetCode Problem 13 Roman to Integer solved

---
+++ Strings/13.roman-to-integer.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=13 lang=python
+#
+# [13] Roman to Integer
+#
+
+# @lc code=start
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        value = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        ans = 0
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and value[s[i]] < value[s[i + 1]]:
+                ans -= value[s[i]]
+            else:
+                ans += value[s[i]]
+
+        return ans
+        
+# @lc code=end
+
