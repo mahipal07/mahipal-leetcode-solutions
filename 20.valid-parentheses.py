fc2c5faa55c471c2644c7684d2824ead8a827e2b
Mahipal Singh <mahipal.singh1k72@gmail.com>
2026-06-29 12:51:09 +0530

    LeetCode Problem 20 valid parentheses solved

---
+++ 20.valid-parentheses.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=20 lang=python
+#
+# [20] Valid Parentheses
+#
+
+# @lc code=start
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+        
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+                
+        return len(stack) == 0
+
+        
+# @lc code=end
+
