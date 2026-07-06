15eebde1d2106486ba38176a9da493756c78c4d3
Mahipal Singh <mahipal.singh1k72@gmail.com>
2026-07-06 18:59:21 +0530

    Daily LeetCode Challenge - 6th July 2026

---
+++ 1288.remove-covered-intervals.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=1288 lang=python3
+#
+# [1288] Remove Covered Intervals
+#
+# 34/34 cases passed (4 ms)
+# Your runtime beats 46.19 % of python3 submissions
+# Your memory usage beats 47.88 % of python3 submissions (19.6 MB)
+
+# @lc code=start
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+        
+        count = 0
+        prev_end = 0
+        
+        for _, end in intervals:
+            if end > prev_end:
+                count += 1
+                prev_end = end
+                
+        return count
+
+
+# @lc code=end
+
