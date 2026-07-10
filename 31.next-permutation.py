de0b9e5f8fb2b9b5a7ec4ada0b03a2dbb0f65993
Mahipal Singh <mahipal.singh1k72@gmail.com>
2026-07-10 18:35:52 +0530

    LeetCode Problem 31 Next Permutation solved

---
+++ 31.next-permutation.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=31 lang=python3
+#
+# [31] Next Permutation
+#
+
+# @lc code=start
+class Solution:
+    def nextPermutation(self, nums: list[int]) -> None:
+        n = len(nums)
+        i = n - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        
+        if i >= 0:
+            j = n - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        left, right = i + 1, n - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+# @lc code=end
+
