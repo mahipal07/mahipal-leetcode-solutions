0da47f38c2b6191a7f523dd58998fbc1f77b7f6b
Mahipal Singh <mahipal.singh1k72@gmail.com>
2026-07-13 09:50:15 +0530

    LeetCode Problem 34 Find First and Last Position of Element in Sorted Array solved

---
+++ 34.find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=34 lang=python3
+#
+# [34] Find First and Last Position of Element in Sorted Array
+#
+
+# @lc code=start
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def findBound(isFirst: bool) -> int:
+            left, right = 0, len(nums) - 1
+            bound = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    bound = mid
+                    if isFirst:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return bound
+
+        return [findBound(True), findBound(False)]
+        
+# @lc code=end
+
