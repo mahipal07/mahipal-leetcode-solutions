d72e191b60140e3ee38322e274ec0810a39fb4aa
Mahipal Singh <mahipal.singh1k72@gmail.com>
2026-07-15 17:48:01 +0530

    Daily LeetCode Challenge - 15th July 2026

---
+++ 3658.gcd-of-odd-and-even-sums.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=3658 lang=python3
+#
+# [3658] GCD of Odd and Even Sums
+#
+# 1000/1000 cases passed (0 ms)
+# Your runtime beats 100 % of python3 submissions
+# Your memory usage beats 53.03 % of python3 submissions (19.2 MB)
+
+# @lc code=start
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        return n
+        
+# @lc code=end
+
