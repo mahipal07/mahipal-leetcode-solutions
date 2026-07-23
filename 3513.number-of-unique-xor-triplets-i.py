e6b0dd9bf63b0b7905d65537fade91271b21a89c
Mahipal Singh <mahipal.singh1k72@gmail.com>
2026-07-23 18:02:46 +0530

    Daily LeetCode Challenge - 23rd July 2026

---
+++ 3513.number-of-unique-xor-triplets-i.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=3513 lang=python3
+#
+# [3513] Number of Unique XOR Triplets I
+#
+
+# @lc code=start
+class Solution:
+
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        return 1 << n.bit_length()
+    
+# @lc code=end
+
