5f85591f1b384927fdedc9a038f43e8fb80031b7
Mahipal Singh <mahipal.singh1k72@gmail.com>
2026-07-23 18:03:30 +0530

    Daily LeetCode Challenge - 23rd July 2026

--- Daily Challenge/3513.number-of-unique-xor-triplets-i.py
+++ Daily Challenge/3513.number-of-unique-xor-triplets-i.py
@@ -2,7 +2,10 @@
 # @lc app=leetcode id=3513 lang=python3
 #
 # [3513] Number of Unique XOR Triplets I
-#
+
+#785/785 cases passed (0 ms)
+# Your runtime beats 100 % of python3 submissions
+# Your memory usage beats 76.27 % of python3 submissions (31.9 MB)
 
 # @lc code=start
 class Solution:
