c271013a8cd8c76b56aa1005d8f6b6038a5ff805
Mahipal Singh <mahipal.singh1k72@gmail.com>
2026-08-02 10:06:33 +0530

    Daily LeetCode Challenge - 2nd August 2026

---
+++ 877.stone-game.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=877 lang=python3
+#
+# [877] Stone Game
+#
+# 46/46 cases passed (0 ms)
+# Your runtime beats 100 % of python3 submissions
+# Your memory usage beats 48.01 % of python3 submissions (19.5 MB)
+
+# @lc code=start
+from typing import List
+
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        return True
+      
+# @lc code=end
+
