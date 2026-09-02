f10cf197ba15a27c97a775891d7ecedace03c0aa
Mahipal Singh <mahipal.singh1k72@gmail.com>
2026-09-02 21:56:59 +0530

    Daily LeetCode Challenge - 2nd September 2026

---
+++ 3875.construct-uniform-parity-array-i.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=3875 lang=python3
+#
+# [3875] Construct Uniform Parity Array I
+#
+# 999/999 cases passed (0 ms)
+# Your runtime beats 100 % of python3 submissions
+# Your memory usage beats 84.34 % of python3 submissions (19.1 MB)
+
+# @lc code=start
+from typing import List
+
+class Solution:
+    def uniformArray(self, nums1: List[int]) -> bool:
+        return True
+
+# @lc code=end
+
