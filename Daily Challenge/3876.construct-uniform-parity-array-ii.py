d903e44db820808766e0eed6a4957c95e46ab55c
Mahipal Singh <mahipal.singh1k72@gmail.com>
2026-09-03 11:41:08 +0530

    Daily LeetCode Challenge - 3rd September 2026

--- Daily Challenge/3876.construct-uniform-parity-array-ii.py
+++ Daily Challenge/3876.construct-uniform-parity-array-ii.py
@@ -3,6 +3,9 @@
 #
 # [3876] Construct Uniform Parity Array II
 #
+# 27/27 cases passed (109 ms)
+# Your runtime beats 52.11 % of python3 submissions
+# Your memory usage beats 28.26 % of python3 submissions (61.4 MB)
 
 # @lc code=start
 class Solution:
